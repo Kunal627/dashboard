ee700481c5d914ae47eff779dbfc60a0ec1afc13
Kunal <kunal.chandra@medtronic.com>
2021-05-09 01:37:35 +0530

    added scatter and

--- dashapp.py
+++ dashapp.py
@@ -8,6 +8,8 @@ from dash.dependencies import Output, Input
 from PIL import Image
 import numpy as np
 import plotly.graph_objects as go
+import copy
+import plotly.io as pio
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY], meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}])
 
@@ -18,7 +20,7 @@ def scattergeo(data):
 
     fig = px.scatter_geo(data, locations="ALPHA3ISO", locationmode="ISO-3",
                         color="STOCK_ABBREV",hover_name="COMPANY", size="REVENUE",color_continuous_scale=px.colors.diverging, 
-                        animation_frame="FY",projection="orthographic",size_max=40, title="Global Revenue",width=500, height=375
+                        animation_frame="FY",projection="orthographic",size_max=40, title="Global Revenue",width=525, height=375
                         )
     
     #fig.add_layout_image(dict(source=img))
@@ -27,15 +29,13 @@ def scattergeo(data):
     return fig
 
 def sankeyplot(data, srccol,destcol,valcol,title):
-    #all_nodes = data[srccol].values.tolist() + data[destcol].values.tolist() + data["FY"].values.tolist()
+
     all_nodes = data[srccol].values.tolist() + data[destcol].values.tolist()
     source_indices = [all_nodes.index(src) for src in data[srccol]]
     target_indices = [all_nodes.index(country) for country in data[destcol]]
     colors = px.colors.qualitative.Pastel
     node_colors_mappings = dict([(node,np.random.choice(colors)) for node in all_nodes])
-    print(node_colors_mappings)
     node_colors = [node_colors_mappings[node] for node in all_nodes]
-    #edge_colors = [node_colors_mappings[node] for node in data["FY"]]
 
     fig = go.Figure(data=[go.Sankey(
         node = dict(
@@ -53,20 +53,65 @@ def sankeyplot(data, srccol,destcol,valcol,title):
           color = node_colors
     ))])
 
-    fig.update_layout(title_text=title, title_x=0.5, font_size=10, template="plotly_dark", width=700, height=375)
+    fig.update_layout(title_text=title, title_x=0.5, font_size=10, template="plotly_dark", width=620, height=375)
     return fig
 
 
 def sunburstplot(data):
-    data["REV_CALC"] = data["REV_CALC"].div(100000000).round(3)
-    data = data.groupby(["FY", "STOCK_ABBREV", "COUNTRY_TRNS" ]).agg({"REV_CALC": "sum"}).reset_index()
-    color = px.colors.sequential
-    fig = px.sunburst(data, path=['FY', 'STOCK_ABBREV','COUNTRY_TRNS'], 
-                  values='REV_CALC', color='STOCK_ABBREV', hover_data=['REV_CALC'], title="Global revenue for financial year (in B $)",
-                  color_continuous_scale=color, range_color =[0,255])
-    fig.update_layout(title_x=0.5,template="plotly_dark")
 
+    fig = px.sunburst(data, path=['FY', 'STOCK_ABBREV', 'CONTINENT','COUNTRY_TRNS'], 
+                  values='REV_CALC', color='UNITS', hover_data=['REV_CALC'], title="Global revenue for financial year (in B $)",
+                  color_continuous_scale='ylgnbu',
+                  color_continuous_midpoint=np.average(data['UNITS'], weights=data['REV_CALC']))
+    fig.update_layout(title_x=0.5, width=682, height=730,template="plotly_dark")
+    #print(fig)
     return fig
+
+
+def barplotfn(data):
+    fig = px.bar(data, x='FY', y='REV_CALC', barmode='group', hover_data=['STOCK_ABBREV', 'REV_CALC', 'COUNTRY_TRNS'], color="STOCK_ABBREV")
+    fig.update_layout(width=682, height=330, template="plotly_dark", title = "Revenue(B$) Trend",title_x=0.5,)
+    return fig
+
+def scatplot(data):
+    fig = px.scatter(data, x="UNIT_PRICE", y="UNITS", size="REV_CALC", color="STOCK_ABBREV",
+                 hover_name="REV_CALC", log_y=True, size_max=60)
+    fig.update_layout(width=455, height=330, template="plotly_dark", title = "Revenue(B$) Trend",title_x=0.5,)
+    return fig
+
+
+card_geo =  dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id="geo-scatter-chart", config={"displayModeBar": False}, className="css-scatter-geo-fig")
+                ])
+            ],className="css-gscat-card")
+    
+    
+card_san =  dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id="sank-chart", config={"displayModeBar": False}, className="css-sankey-fig")
+                ])
+            ],className="css-sankey-card")
+    
+    
+card_sun =  dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id="sunbrst-chart", config={"displayModeBar": False}, className="css-sunbrst-fig")
+                ])
+            ],className="css-sunbrst-card")
+
+
+card_bar =  dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id="barplot-chart", config={"displayModeBar": False}, className="css-barplot-fig")
+                ])
+            ], className="css-barplot-card")
+
+card_scat =  dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id="scatter-chart", config={"displayModeBar": False}, className="css-scatter-fig")
+                ])
+            ], className="css-scatter-card")
 
 
 app.layout = dhc.Div([
@@ -85,10 +130,10 @@ app.layout = dhc.Div([
         dbc.CardBody([
 
             dbc.Row([
-                dbc.Col([dhc.H6("Financial Year", className="text-center")], width={'size':3, "offset": 0}),
-                dbc.Col([dhc.H6("Company", className="text-center")], width=3),
-                dbc.Col([dhc.H6("Country", className="text-center")], width=3),
-                dbc.Col([dhc.H6("Region", className="text-center")], width=3)
+                dbc.Col([dhc.H6("Financial Year", className="text-center")], width={'size':3, "offset": 0}, className="css-row-dd-head"),
+                dbc.Col([dhc.H6("Company", className="text-center")], width=3, className="css-row-dd-head"),
+                dbc.Col([dhc.H6("Country", className="text-center")], width=3, className="css-row-dd-head"),
+                dbc.Col([dhc.H6("Region", className="text-center")], width=3, className="css-row-dd-head")
             ], align='center'),
 
             dbc.Row([
@@ -125,29 +170,21 @@ app.layout = dhc.Div([
 
     dhc.Br(),
 
-    dbc.Card([
+    dbc.Row([
 
-        dbc.CardBody([
+        dbc.Col(card_geo, width={'size':4, "offset": 0 }),
+        dbc.Col(card_san, width={'size':4, "offset": 0 }),    
+        dbc.Col(card_sun, width=4)
 
-            dbc.Row([
+    ], justify="around", className="css-plot-row1"),
 
-                dbc.Col([
-                    dcc.Graph(id="geo-scatter-chart", config={"displayModeBar": False}, className="css-scatter-geo-fig")
-                ], width={'size':3.5, "offset": 0 }, className="css-geo-scatter"),
+    dhc.Br(),
 
-                dbc.Col([
-                    dcc.Graph(id="sank-chart", config={"displayModeBar": False}, className="css-sankey-fig")
-                ], width={'size':5, "offset": 0 }),
+    dbc.Row([
+        dbc.Col(card_bar, width={'size':5, "offset": 0 }),
+        dbc.Col(card_scat,width={'size':3})
+    ])
 
-                dbc.Col([
-                    dcc.Graph(id="sunbrst-chart", config={"displayModeBar": False}, className="css-sunbrst-fig")
-                ], width={'size':3.5, "offset": 0 })
-
-            ])
-
-        ])
-
-    ],className="css-chart1-card")
 
 ])
 
@@ -161,10 +198,8 @@ app.layout = dhc.Div([
 )
 def update_graph(fy, tic, cntry):
 
-    #print(fy, tic,cntry)
     filtered_data = data[(data["FY"].isin(fy)) & (data["STOCK_ABBREV"].isin(tic)) & (data["COUNTRY_TRNS"].isin(cntry))]
     fig_scatter_geo = scattergeo(filtered_data)
-
     return fig_scatter_geo
 
 @app.callback(
@@ -193,10 +228,37 @@ def update_sankey(fy, tic, cntry):
         Input("id-drp-fy", "value")
     ],
 )
-def update_sankey(fy):
-    dataset = data
-    fig = sunburstplot(dataset)
-    return fig
+def update_sunbrst(fy):
+    dataset = pd.read_csv('./data/output/finaldatav01.csv')
+    dataset.drop(columns=['COMPANY', 'PRODUCT_ID', 'PRODUCT_SUBTYPE', 'UNIT_PRICE','Unnamed: 9', 'Unnamed: 10','REVENUE','ALPHA3ISO','COUNTRY'], inplace=True)
+    dataset["REV_CALC"] = dataset["REV_CALC"].div(1000000000).round(3)
+    fig_snbrst = sunburstplot(dataset)
+    return fig_snbrst
+
+@app.callback(
+    Output("barplot-chart", 'figure'),
+    [
+        Input("id-drp-cntry", "value"),
+    ],
+)
+def update_barplot(cntry):
+    dataset = pd.read_csv('./data/output/finaldatav01.csv')
+    filtered_data = dataset[dataset["COUNTRY_TRNS"].isin(cntry)]
+    filtered_data["REV_CALC"] = filtered_data["REV_CALC"].div(1000000000).round(3)
+    fig_bar = barplotfn(filtered_data)
+    return fig_bar
+
+
+@app.callback(
+    Output("scatter-chart", 'figure'),
+    [
+        Input("id-drp-cntry", "value"),
+    ],
+)
+def update_barplot(cntry):
+    dataset = pd.read_csv('./data/output/finaldatav01.csv')
+    fig_scat = scatplot(dataset)
+    return fig_scat
 
 
 
